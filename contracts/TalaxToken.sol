ea0ac339df8ebe77b1e11ac031edcda576ab8c00
evan533 <rianto.ev@gmail.com>
2022-06-09 10:29:21 +0700

    change immutabe to sontant

--- contracts/TalaxToken.sol
+++ contracts/TalaxToken.sol
@@ -42,27 +42,27 @@ contract TalaxToken is ERC20, ERC20Burnable, Ownable, Stakable, Multilockable {
      * address liquidity_reserve_address;
      */
 
-    address public immutable public_sale_address =
+    address public constant public_sale_address =
         0x5470c8FF25EC05980fc7C2967D076B8012298fE7;
-    address public immutable private_placement_address =
+    address public constant private_placement_address =
         0x07A20dc6722563783e44BA8EDCA08c774621125E;
-    address public immutable dev_pool_address_1 =
+    address public constant dev_pool_address_1 =
         0xf09f65dD4D229E991901669Ad7c7549f060E30b9;
-    address public immutable dev_pool_address_2 =
+    address public constant dev_pool_address_2 =
         0x1A2118E056D6aF192E233C2c9CFB34e067DED1F8;
-    address public immutable dev_pool_address_3 =
+    address public constant dev_pool_address_3 =
         0x126974fa373267d86fAB6d6871Afe62ccB68e810;
-    address public immutable strategic_partner_address_1 =
+    address public constant strategic_partner_address_1 =
         0x2F838cF0Df38b2E91E747a01ddAE5EBad5558b7A;
-    address public immutable strategic_partner_address_2 =
+    address public constant strategic_partner_address_2 =
         0x45094071c4DAaf6A9a73B0a0f095a2b138bd8A3A;
-    address public immutable strategic_partner_address_3 =
+    address public constant strategic_partner_address_3 =
         0xAeB26fB84d0E2b3B353Cd50f0A29FD40C916d2Ab;
-    address public immutable team_and_project_coordinator_address_1 =
+    address public constant team_and_project_coordinator_address_1 =
         0xE61E7a7D16db384433E532fB85724e7f3BdAaE2F;
-    address public immutable team_and_project_coordinator_address_2 =
+    address public constant team_and_project_coordinator_address_2 =
         0x406605Eb24A97A2D61b516d8d850F2aeFA6A731a;
-    address public immutable team_and_project_coordinator_address_3 =
+    address public constant team_and_project_coordinator_address_3 =
         0x97620dEAdC98bC8173303686037ce7B986CF53C3;
 
     /* ------------------------------------------ Lockable ------------------------------------------ */
