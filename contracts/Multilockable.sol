baec1564682a9578336992eac1bafbce27b46e54
Evan <rianto.ev@gmail.com>
2022-07-19 09:02:32 +0700

    change variable format

--- contracts/Multilockable.sol
+++ contracts/Multilockable.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Multilockable {
     using SafeMath for uint256;
-    uint256 public ;
+    uint256 public privateSaleUsers;
 
     uint256 public totalAmount = 14679000 * 1e18;
     uint256 public constant phase_1_total = 733950 * 1e18;
