28cc826b359e0c5b6920cbeaf8b78d61e0d65c10
Evan <rianto.ev@gmail.com>
2022-11-17 09:13:25 +0700

    restructure code

---
+++ contracts/Factory/EscrowFactory.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.11;
+
+import "./ProjectNameEscrow.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract EscrowFactory is Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _counter;
+
+    mapping(uint256 => address) public projects;
+    address immutable _masterContract;
+
+    constructor() {
+        _masterContract = address(new ProjectNameEscrow());
+    }
+
+    function getCurrentCounter() external view returns (uint256) {
+        return _counter.current();
+    }
+
+    function createProject(address tokenAddress)
+        external
+        onlyOwner
+        returns (address)
+    {
+        address project = Clones.clone(_masterContract);
+        ProjectNameNFT(project).init(tokenAddress);
+        projects[_counter.current()] = project;
+        _counter.increment();
+        return project;
+    }
+
+    function getProject(uint256 counterIdx) external view returns (address) {
+        return projects[counterIdx];
+    }
+}
