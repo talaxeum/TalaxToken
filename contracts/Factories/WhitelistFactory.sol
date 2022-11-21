f2b18efacbc1a427a972bb4dd6d34b8b8a966e29
Evan <rianto.ev@gmail.com>
2022-11-21 14:13:56 +0700

    add several factories

---
+++ contracts/Factories/WhitelistFactory.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "../ERC20/Whitelist.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract WhitelistFactory is Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _counter;
+
+    mapping(uint256 => address) public whitelists;
+    address immutable _masterContract;
+
+    constructor() {
+        _masterContract = address(new Whitelist());
+    }
+
+    function getCurrentCounter() external view returns (uint256) {
+        return _counter.current();
+    }
+
+    function createWhitelist(
+        address token,
+        uint64 start,
+        uint64 duration,
+        uint64 cliff
+    ) external onlyOwner returns (address) {
+        address whitelist = Clones.clone(_masterContract);
+        Whitelist(whitelist).init(token, start, duration, cliff);
+        whitelists[_counter.current()] = whitelist;
+        _counter.increment();
+        return whitelist;
+    }
+
+    function getWhitelist(uint256 counterIdx) external view returns (address) {
+        return whitelists[counterIdx];
+    }
+}
