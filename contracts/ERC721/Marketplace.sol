503a2725836fd2751a5ba41206bc251a4e0dcaae
Evan <rianto.ev@gmail.com>
2022-11-23 16:56:26 +0700

    add offering functions for unlisted NFT

--- contracts/ERC721/Marketplace.sol
+++ contracts/ERC721/Marketplace.sol
@@ -10,6 +10,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 error PriceNotMet(address nftAddress, uint256 tokenId, uint256 price);
 error ItemNotForSale(address nftAddress, uint256 tokenId);
 error NotListed(address nftAddress, uint256 tokenId);
+error Listed(address nftAddress, uint256 tokenId);
 error AlreadyListed(address nftAddress, uint256 tokenId);
 error NoProceeds();
 error NotOwner();
@@ -61,6 +62,25 @@ contract NFTMarketplace is ReentrancyGuard {
         uint256 indexed tokenId
     );
     event BidAccepted(
+        address seller,
+        address indexed buyer,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+    event OfferCreated(
+        address indexed buyer,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 offeringPrice
+    );
+    event OfferCancelled(
+        address indexed buyer,
+        address indexed nftAddress,
+        uint256 indexed tokenId
+    );
+    event OfferAccepted(
+        address seller,
         address indexed buyer,
         address indexed nftAddress,
         uint256 indexed tokenId,
@@ -70,9 +90,11 @@ contract NFTMarketplace is ReentrancyGuard {
     address private token;
 
     mapping(address => mapping(uint256 => mapping(address => Bid)))
+        private s_biddings;
+    mapping(address => mapping(uint256 => mapping(address => Bid)))
         private s_offerings;
     mapping(address => mapping(uint256 => Listing)) private s_listings;
-    mapping(address => uint256) private s_proceeds;
+    // mapping(address => uint256) private s_proceeds;
 
     modifier notListed(address nftAddress, uint256 tokenId) {
         Listing memory listing = s_listings[nftAddress][tokenId];
@@ -87,8 +109,19 @@ contract NFTMarketplace is ReentrancyGuard {
         if (listing.price <= 0) {
             revert NotListed(nftAddress, tokenId);
         }
-        if (listing.endDate > block.timestamp) {
+        if (listing.endDate < block.timestamp) {
             revert NotListed(nftAddress, tokenId);
+        }
+        _;
+    }
+
+    modifier isNotListed(address nftAddress, uint256 tokenId) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (listing.price >= 0) {
+            revert Listed(nftAddress, tokenId);
+        }
+        if (listing.endDate > block.timestamp) {
+            revert Listed(nftAddress, tokenId);
         }
         _;
     }
@@ -186,62 +219,6 @@ contract NFTMarketplace is ReentrancyGuard {
         emit ItemCanceled(msg.sender, nftAddress, tokenId);
     }
 
-    function createBid(
-        address nftAddress,
-        uint256 tokenId,
-        uint256 price
-    )
-        external
-        payable
-        isListed(nftAddress, tokenId)
-        isNotOwner(nftAddress, tokenId, msg.sender)
-        nonReentrant
-    {
-        if (price <= 0) {
-            revert PriceMustBeAboveZero();
-        }
-
-        s_offerings[nftAddress][tokenId][msg.sender] = Bid(price, msg.sender);
-        emit BidCreated(msg.sender, nftAddress, tokenId, price);
-    }
-
-    function cancelBid(address nftAddress, uint256 tokenId)
-        external
-        isListed(nftAddress, tokenId)
-        nonReentrant
-    {
-        delete (s_offerings[nftAddress][tokenId][msg.sender]);
-        emit BidCancelled(msg.sender, nftAddress, tokenId);
-    }
-
-    function acceptBid(
-        address nftAddress,
-        uint256 tokenId,
-        address buyer
-    )
-        external
-        isListed(nftAddress, tokenId)
-        nonReentrant
-        isOwner(nftAddress, tokenId, msg.sender)
-    {
-        Listing memory listedItem = s_listings[nftAddress][tokenId];
-        Bid memory bidding = s_offerings[nftAddress][tokenId][buyer];
-        delete (s_listings[nftAddress][tokenId]);
-        delete (s_offerings[nftAddress][tokenId][buyer]);
-        SafeERC20.safeTransferFrom(
-            IERC20(token),
-            bidding.buyer,
-            msg.sender,
-            bidding.price
-        );
-        IERC721(nftAddress).safeTransferFrom(
-            listedItem.seller,
-            msg.sender,
-            tokenId
-        );
-        emit BidAccepted(msg.sender, nftAddress, tokenId, listedItem.price);
-    }
-
     /**
      * @notice Method for buying listing
      * @notice The owner of an NFT could unapprove the marketplace,
@@ -308,6 +285,172 @@ contract NFTMarketplace is ReentrancyGuard {
         emit ItemListed(msg.sender, nftAddress, tokenId, newPrice, newEndDate);
     }
 
+    /* ---------------------------------------------------------------------------------------------- */
+    /*                                        Bidding Functions                                       */
+    /* ---------------------------------------------------------------------------------------------- */
+
+    /**
+     * @notice Method for create Bidding (Bid on a listed NFT)
+     * @param nftAddress Address of NFT contract
+     * @param tokenId Token ID of NFT
+     * @param price Amount of placed bid
+     */
+    function createBid(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 price
+    )
+        external
+        payable
+        isListed(nftAddress, tokenId)
+        isNotOwner(nftAddress, tokenId, msg.sender)
+        nonReentrant
+    {
+        if (price <= 0) {
+            revert PriceMustBeAboveZero();
+        }
+
+        s_biddings[nftAddress][tokenId][msg.sender] = Bid(price, msg.sender);
+        emit BidCreated(msg.sender, nftAddress, tokenId, price);
+    }
+
+    /**
+     * @notice Method to cancel Bid
+     * @param nftAddress Address of NFT contract
+     * @param tokenId Token ID of NFT
+     */
+    function cancelBid(address nftAddress, uint256 tokenId)
+        external
+        isListed(nftAddress, tokenId)
+        nonReentrant
+    {
+        delete (s_biddings[nftAddress][tokenId][msg.sender]);
+        emit BidCancelled(msg.sender, nftAddress, tokenId);
+    }
+
+    /**
+     * @notice Method to accept Bid (Only callable by owner of NFT)
+     * @param nftAddress Address of NFT contract
+     * @param tokenId Token ID of NFT
+     * @param buyer The buyer/bidder of the selected bid
+     */
+    function acceptBid(
+        address nftAddress,
+        uint256 tokenId,
+        address buyer
+    )
+        external
+        isListed(nftAddress, tokenId)
+        nonReentrant
+        isOwner(nftAddress, tokenId, msg.sender)
+    {
+        Listing memory listedItem = s_listings[nftAddress][tokenId];
+        Bid memory bidding = s_biddings[nftAddress][tokenId][buyer];
+        delete (s_listings[nftAddress][tokenId]);
+        delete (s_biddings[nftAddress][tokenId][buyer]);
+        SafeERC20.safeTransferFrom(
+            IERC20(token),
+            bidding.buyer,
+            msg.sender,
+            bidding.price
+        );
+        IERC721(nftAddress).safeTransferFrom(
+            listedItem.seller,
+            msg.sender,
+            tokenId
+        );
+        emit BidAccepted(
+            listedItem.seller,
+            msg.sender,
+            nftAddress,
+            tokenId,
+            listedItem.price
+        );
+    }
+
+    /* ---------------------------------------------------------------------------------------------- */
+    /*                                       Offering Functions                                       */
+    /* ---------------------------------------------------------------------------------------------- */
+
+    /**
+     * @notice Method for create Bidding (Bid on a listed NFT)
+     * @param nftAddress Address of NFT contract
+     * @param tokenId Token ID of NFT
+     * @param price Amount of placed bid
+     */
+    function createOffer(
+        address nftAddress,
+        uint256 tokenId,
+        uint256 price
+    )
+        external
+        payable
+        isNotListed(nftAddress, tokenId)
+        isNotOwner(nftAddress, tokenId, msg.sender)
+        nonReentrant
+    {
+        if (price <= 0) {
+            revert PriceMustBeAboveZero();
+        }
+
+        s_offerings[nftAddress][tokenId][msg.sender] = Bid(price, msg.sender);
+        emit OfferCreated(msg.sender, nftAddress, tokenId, price);
+    }
+
+    /**
+     * @notice Method to cancel Bid
+     * @param nftAddress Address of NFT contract
+     * @param tokenId Token ID of NFT
+     */
+    function cancelOffer(address nftAddress, uint256 tokenId)
+        external
+        isNotListed(nftAddress, tokenId)
+        nonReentrant
+    {
+        delete (s_offerings[nftAddress][tokenId][msg.sender]);
+        emit OfferCancelled(msg.sender, nftAddress, tokenId);
+    }
+
+    /**
+     * @notice Method to accept Bid (Only callable by owner of NFT)
+     * @param nftAddress Address of NFT contract
+     * @param tokenId Token ID of NFT
+     * @param buyer The buyer/bidder of the selected bid
+     */
+    function acceptOffer(
+        address nftAddress,
+        uint256 tokenId,
+        address buyer
+    )
+        external
+        isNotListed(nftAddress, tokenId)
+        nonReentrant
+        isOwner(nftAddress, tokenId, msg.sender)
+    {
+        Listing memory listedItem = s_listings[nftAddress][tokenId];
+        Bid memory bidding = s_offerings[nftAddress][tokenId][buyer];
+        delete (s_listings[nftAddress][tokenId]);
+        delete (s_offerings[nftAddress][tokenId][buyer]);
+        SafeERC20.safeTransferFrom(
+            IERC20(token),
+            bidding.buyer,
+            msg.sender,
+            bidding.price
+        );
+        IERC721(nftAddress).safeTransferFrom(
+            listedItem.seller,
+            msg.sender,
+            tokenId
+        );
+        emit OfferAccepted(
+            listedItem.seller,
+            msg.sender,
+            nftAddress,
+            tokenId,
+            listedItem.price
+        );
+    }
+
     /**
      * @notice Method for withdrawing proceeds from sales
      */
@@ -333,7 +476,7 @@ contract NFTMarketplace is ReentrancyGuard {
         return s_listings[nftAddress][tokenId];
     }
 
-    function getProceeds(address seller) external view returns (uint256) {
-        return s_proceeds[seller];
-    }
+    // function getProceeds(address seller) external view returns (uint256) {
+    //     return s_proceeds[seller];
+    // }
 }
